6d4a8444684ccb43abce3d14467b8bf733e63bc9
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-08-12 21:02:25 +0100

    bug fix

--- src/utils/HTMLGenerator.js
+++ src/utils/HTMLGenerator.js
@@ -4,6 +4,6 @@ const generateHTML = ({ manager, engineer, intern }) => {
   const html = `<html>
   <body>Hello</body>
 </html>`;
-  fs.writeFileSync(path.join(__dirname, "../dist", "index.html"));
+  fs.writeFileSync(path.join(__dirname, "../dist", "index.html"), html);
 };
 module.exports = generateHTML;
