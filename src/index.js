9aac8f60103edc51c143727643bdbf50a13e760f
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-08-12 20:54:04 +0100

    Update index page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,65 @@
-const init = () => {
-  console.log("hello");
+const inquirer = require("inquirer");
+const Manager = require("./lib/classes/Manager");
+const managerQuestions = require("./lib/questions/managerQuestions");
+const engineerQuestions = require("./lib/questions/engineerQuestions");
+const internQuestions = require("./lib/questions/internQuestions");
+const generateHTML = require("./utils/HTMLGenerator");
+const options = [
+  {
+    type: "list",
+    message: "What operation do you want to perform?",
+    name: "choice",
+    choices: [
+      {
+        name: "Create Engineer",
+        value: "engineer",
+        loop: true,
+      },
+      {
+        name: "Create Intern",
+        value: "intern",
+        loop: true,
+      },
+      {
+        name: "Exit the app",
+        value: "quit",
+        loop: false,
+      },
+    ],
+  },
+];
+
+const init = async () => {
+  const employees = {
+    manager: [],
+    intern: [],
+    engineer: [],
+  };
+  const managerAnswers = await inquirer.prompt(managerQuestions);
+  const manager = new Manager(
+    managerAnswers.name,
+    managerAnswers.email,
+    managerAnswers.employeeID,
+    managerAnswers.officeNumber
+  );
+  employees.manager.push(manager);
+  let inProgress = true;
+  while (inProgress) {
+    const { choice } = await inquirer.prompt(options);
+    if (choice === "quit") {
+      inProgress = false;
+    }
+    if (choice === "engineer") {
+      const engineer = await inquirer.prompt(engineerQuestions);
+      employees.engineer.push(engineer);
+    }
+    if (choice === "intern") {
+      const intern = await inquirer.prompt(internQuestions);
+      employees.intern.push(intern);
+    }
+  }
+  console.log(managerAnswers);
+  console.log(employees);
+  generateHTML(employees);
 };
 init();
